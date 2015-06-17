56b43bb9335309de4cc49d390095e08b9765b5c9
拔赤 <jay.li@alibaba-inc.com>
2015-06-17 15:36:33 +0800

    add all

---
+++ javascripts/disqus.js
@@ -0,0 +1,10 @@
+/* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+var disqus_shortname = 'jayliblog'; // required: replace example with your forum shortname
+var disqus_identifier = 'urn:uuid:0597dfb0-02c0-40e1-82f8-176b6dc31c8a';
+
+/* * * DON'T EDIT BELOW THIS LINE * * */
+(function() {
+    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+    dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+})();
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,11 @@
 console.log('This would be the main JS file.');
+
+
+$('header h1').grumble(
+	{
+		text: 'Kissy-tastic!', 
+		angle: 85, 
+		distance: 100, 
+		showAfter: 500
+	}
+);
