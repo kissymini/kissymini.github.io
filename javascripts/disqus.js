473379ad43f4b5000e751a4624f8324246e361be
拔赤 <jay.li@alibaba-inc.com>
2015-08-24 17:18:03 +0800

    add all

--- javascripts/disqus.js
+++ javascripts/disqus.js
@@ -1,10 +1,12 @@
-/* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
-var disqus_shortname = 'jayliblog'; // required: replace example with your forum shortname
-var disqus_identifier = 'urn:uuid:0597dfb0-02c0-40e1-82f8-176b6dc31c8a';
+
+
+
+/* * * CONFIGURATION VARIABLES * * */
+var disqus_shortname = 'mobilekissyuicom';
 
 /* * * DON'T EDIT BELOW THIS LINE * * */
 (function() {
-    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-    dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
-    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+	var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+	dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+	(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
 })();
